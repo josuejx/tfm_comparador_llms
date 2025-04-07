36fdf36076ba9630012909ee374c683d6ed998ed
JOSUE GARCIA ASENSI <josue.garcia.asensi.contractor@bbva.com>
2025-04-07 17:25:44 +0200

    feat: new app name

--- src/components/layout/TopBar.vue
+++ src/components/layout/TopBar.vue
@@ -8,7 +8,10 @@ import { isDarkMode } from '@/utils/utils';
     <div class="flex items-center gap-2">
       <img src="@/assets/EvaLLM.svg" alt="EvaLLM" class="w-8 h-8" v-if="!isDarkMode" />
       <img src="@/assets/EvaLLM_white.svg" alt="EvaLLM" class="w-8 h-8" v-else />
-      <h1 class="text-2xl font-bold text-theme-steelblue dark:text-white">Comparador LLMs</h1>
+      <div class="flex flex-col">
+        <h1 class="text-2xl font-bold text-theme-steelblue dark:text-white">EvaLLM</h1>
+        <p class="text-gray-500 dark:text-gray-300 text-[10px]">Comparador de LLMs</p>
+      </div>
     </div>
     <DarkModeToggler />
   </div>
