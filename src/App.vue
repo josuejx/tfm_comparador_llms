df4381f4175c142fff08b13e223435d16db97620
JOSUE GARCIA ASENSI <josue.garcia.asensi.contractor@bbva.com>
2025-04-09 08:20:29 +0200

    feat: added animations

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import TopBar from "@/components/layout/TopBar.vue";
 
 <template>
   <div class="flex flex-col min-h-screen max-h-screen dark:text-white">
-    <TopBar />
-    <div class="grow flex p-4 gap-4 overflow-y-auto overflow-x-hidden">
+    <TopBar class="animate-slide-in-top" />
+    <div id="router-content" class="grow flex p-4 gap-4 overflow-y-auto overflow-x-hidden animate-zoom-in">
       <RouterView />
     </div>
   </div>
