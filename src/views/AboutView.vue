320d410708dbb2cfd4de8f9be9b324cc20be6693
JOSUE GARCIA ASENSI <josue.garcia.asensi.contractor@bbva.com>
2025-04-09 13:58:48 +0200

    feat: nuevo diseño

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,23 +6,25 @@ import { isDarkMode } from "@/utils/utils";
 <template>
     <div class="w-full">
         <div
-            class="h-full bg-white dark:bg-theme-midnight rounded-2xl shadow-lg flex flex-col items-center p-4 gap-4">
-            <logo_unir :color="isDarkMode ? '#FFF' : '#000'" class="size-72" />
-            <div class="text-center">
-                <p class="text-lg">Universidad Internacional de la Rioja (UNIR)</p>
-                <p class="text-2xl font-bold">Escuela Superior de Ingeniería y Tecnología</p>
-                <p class="text-lg">Master en Inteligencia Artificial y Aplicaciones</p>
-            </div>
-            <p class="text-2xl text-theme-seablue font-bold text-center">Desarrollo de una Plataforma Web para la Experimentación
+            class="h-full bg-white dark:bg-theme-midnight rounded-2xl shadow-lg flex flex-col justify-between p-4 gap-4">
+            <div class="flex flex-col items-center">
+                <logo_unir :color="isDarkMode ? '#FFF' : '#000'" class="w-60" />
+                <p class="text-3xl font-bold">Trabajo Fin de Master (TFM)</p>
+                <p class="text-2xl text-theme-seablue font-bold text-center">Desarrollo de una Plataforma Web para la
+                Experimentación
                 con Modelos de Análisis de Sentimientos</p>
+            </div>
             <div class="text-center">
-                <p class="text-lg">Trabajo Fin de Master (TFM)</p>
                 <p><strong>Presentado por:</strong></p>
                 <p class="text-lg">Josué García Asensi</p>
-                <p><strong>Dirigido por:</strong></p>
+                <p class="mt-14"><strong>Dirigido por:</strong></p>
                 <p class="text-lg">Ane Iturzaeta Agirre</p>
             </div>
-            <p class="text-lg text-center">Julio 2025</p>
+            <div class="text-right">
+                <p class="text-lg">Universidad Internacional de la Rioja (UNIR)</p>
+                <p class="text-lg">Master en Inteligencia Artificial y Aplicaciones</p>
+                <p class="text-lg">Julio 2025</p>
+            </div>
         </div>
     </div>
 </template>
