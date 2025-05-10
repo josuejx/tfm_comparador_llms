50bb98229afc69829b1dbe95c4de92d3556bc70b
josuejx <josue.ga00@gmail.com>
2025-05-10 23:23:15 +0200

    feat: open ai service

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,3 +1,4 @@
 const HF_TOKEN = "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";
+const OPENAI_API_KEY = "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";
 
-export { HF_TOKEN };
+export { HF_TOKEN, OPENAI_API_KEY };
---
+++ src/services/openai.services.ts
@@ -0,0 +1,8 @@
+import type Message from "@/types/message";
+import { OPENAI_API_KEY } from "./env";
+
+export class OpenAIService {
+  static async analyzeModelResults(messages: Message[]) {
+    
+  }
+}
