6c004940dde4f026df27f79a796612ab54f638d6
JOSUE GARCIA ASENSI <josue.garcia.asensi.contractor@bbva.com>
2025-10-01 08:53:47 +0200

    fix: correccion errores

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,4 +1,4 @@
-const HF_TOKEN = import.meta.env.HF_TOKEN;
-const OPENAI_API_KEY = import.meta.env.OPENAI_API_KEY;
+const HF_TOKEN = import.meta.env.VITE_HF_TOKEN;
+const OPENAI_API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
 
 export { HF_TOKEN, OPENAI_API_KEY };
--- src/services/huggingface.services.ts
+++ src/services/huggingface.services.ts
@@ -2,20 +2,22 @@ import { InferenceClient } from "@huggingface/inference";
 import { HF_TOKEN } from "./env";
 
 const HuggingFaceModels = [
-  { id: "mistralai/Mistral-7B-Instruct-v0.3", name: "Mistral" },
-  { id: "meta-llama/Llama-3.1-8B-Instruct", name: "Llama 3.1" },
-  { id: "Qwen/Qwen3-235B-A22B", name: "Qwen 3" },
-  { id: "google/gemma-3-27b-it", name: "Gemma 3" },
-  { id: "deepseek-ai/DeepSeek-V3.2-Exp", name: "DeepSeek V3.2" },
+  // { id: "mistralai/Mistral-7B-Instruct-v0.3", provider: 'hf-inference', name: "Mistral" },
+  { id: "meta-llama/Llama-3.1-8B-Instruct", provider: 'fireworks-ai', name: "Llama 3.1" },
+  { id: "Qwen/Qwen3-235B-A22B", provider: 'fireworks-ai', name: "Qwen 3" },
+  { id: "google/gemma-3-27b-it", provider: 'nebius', name: "Gemma 3" },
+  { id: "deepseek-ai/DeepSeek-V3.2-Exp", provider: 'novita', name: "DeepSeek V3.2" },
 ];
 
 class HuggingFaceService {
   static async queryLLM(input: string, model: string, promptSystem: string = "") {
     try {
+      console.log("HF_TOKEN:", HF_TOKEN);
       const client = new InferenceClient(HF_TOKEN);
+      const modelProvider = HuggingFaceModels.find(m => m.id === model)?.provider || 'hf-inference';
 
       const chatCompletion = await client.chatCompletion({
-        provider: "hf-inference",
+        provider: modelProvider as any,
         model: model,
         messages: [
           {
