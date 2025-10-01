031385418fa0eeef3ac7ea5379a2fc516a83a43c
JOSUE GARCIA ASENSI <josue.garcia.asensi.contractor@bbva.com>
2025-10-01 08:31:52 +0200

    fix: ocultar api keys

--- src/services/env.ts
+++ src/services/env.ts
@@ -1,4 +1,4 @@
-const HF_TOKEN = "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";
-const OPENAI_API_KEY = "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";
+const HF_TOKEN = import.meta.env.HF_TOKEN;
+const OPENAI_API_KEY = import.meta.env.OPENAI_API_KEY;
 
 export { HF_TOKEN, OPENAI_API_KEY };
